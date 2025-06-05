96f9dc52aa978007a3d1636cea26d86fc2393c57
Mukul Kumar <mukulrao6480@gmail.com>
2025-06-05 10:43:08 +0530

    add condition attiribute in bmerge and catch it and show mssg through handler in merge

--- R/bmerge.R
+++ R/bmerge.R
@@ -84,12 +84,8 @@ bmerge = function(i, x, icols, xcols, roll, rollends, nomatch, mult, ops, verbos
           next
         }
       }
-      condition_message <- sprintf(
-        "Incompatible join types: %s (%s) and %s (%s). Factor columns must join to factor or character columns.", # Exact match for tests like 2044.24
-        xname, x_merge_type,
-        iname, i_merge_type
-      )
-      condition <- structure(
+      condition_message = sprintf("Incompatible join types: %s (%s) and %s (%s). Factor columns must join to factor or character columns.", xname, x_merge_type, iname, i_merge_type)
+      condition = structure(
         list(
           message = condition_message,
           bmerge_x_arg_col_name      = names(x)[xcol],
